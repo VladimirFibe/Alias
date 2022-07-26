efb0d42c6bcaccf3b89ca6b6ea7d71693eb85daf
Alexander Altman <qewhouse@yandex.ru>
2022-07-26 22:59:36 +0300

    Main screen design

---
+++ Alias/Controller/MainScreenViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alias
+//
+//  Created by Vladimir Fibe on 26.07.2022.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func rulesButtonPressed(_ sender: UIButton) {
+      
+    }
+    
+    }
+
