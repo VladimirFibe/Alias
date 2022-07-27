9d3af8725487d9ffbba666d667196cfe90e580b5
Alexander Altman <qewhouse@yandex.ru>
2022-07-27 14:28:37 +0300

    2 screen

--- Alias/Controller/RulesViewController.swift
+++ Alias/Controller/RulesViewController.swift
@@ -15,13 +15,11 @@ class RulesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
     @IBAction func backButtonPressed(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
-    
-
+   
 }
