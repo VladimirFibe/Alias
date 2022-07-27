dfe3148f5b65900d7d506ebd5fc73993f5a46680
Alexander Altman <qewhouse@yandex.ru>
2022-07-27 14:23:00 +0300

    rules screen

--- Alias/Controller/RulesViewController.swift
+++ Alias/Controller/RulesViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class RulesViewController: UIViewController {
 
+    @IBOutlet weak var rulesScrollView: UIScrollView!
+    @IBOutlet weak var rulesLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,14 +22,6 @@ class RulesViewController: UIViewController {
     @IBAction func backButtonPressed(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
