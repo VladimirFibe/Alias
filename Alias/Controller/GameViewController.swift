674f3e97119c1bd8b82812f1352362b597335f1e
Roman <northrome29@gmail.com>
2022-07-28 13:10:04 +0200

    game screen

---
+++ Alias/Controller/GameViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GameViewController.swift
+//  Alias
+//
+//  Created by Roman on 28.07.2022.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- Alias/Controller/ThemesViewController.swift
+++ Alias/Controller/ThemesViewController.swift
@@ -24,6 +24,8 @@ class ThemesViewController: UIViewController {
     
     @IBAction func buttonPressed(_ sender: Any) { //общая функция для всех кнопок
         
+        self.performSegue(withIdentifier: "goToGame", sender: self)
+        
         var words = Words()
         print((sender as AnyObject).currentTitle)
         words.defineWordsArray(topic: (sender as AnyObject).currentTitle!) //определяем массив слов
@@ -55,16 +57,5 @@ class ThemesViewController: UIViewController {
 
         
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
