8ded5832fce0db8675853c8418f2e47e97c0c6cb
Alexander Altman <qewhouse@yandex.ru>
2022-07-28 20:39:08 +0300

    game screen timer + sound

--- Alias/Controller/GameViewController.swift
+++ Alias/Controller/GameViewController.swift
@@ -6,11 +6,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var timerBar: UIProgressView!
+    @IBOutlet weak var wordLable: UILabel!
+    
+    let roundTimer = 60
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+    
+    
+    
+    @IBAction func skipButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func failButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func yesButton(_ sender: UIButton) {
+    }
 
+
+
+func updateUI() {
+    timer.invalidate()
+    totalTime = roundTimer
+    timerBar.setProgress(0.0, animated: true)
+    secondsPassed = 0
+    timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    
+}
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            print(percentageProgress)
+            timerBar.progress = percentageProgress
+            secondsPassed += 1
+        } else {
+            timer.invalidate()
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension:"mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+        }
     }
 }
