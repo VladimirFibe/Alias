f8556bf444aa7ab14708a83a048e74612f6b07cd
Vladimir <motiw@icloud.com>
2022-08-21 07:10:59 +0600

    добавил перемещение карты

--- Alias/ViewController.swift
+++ Alias/ViewController.swift
@@ -9,17 +9,27 @@ import SwiftUI
 
 class ViewController: UIViewController {
   let cardView = UIView()
+  var panGesture = UIPanGestureRecognizer()
   override func viewDidLoad() {
     super.viewDidLoad()
     view.addSubview(cardView)
     cardView.backgroundColor = .systemBlue
     cardView.translatesAutoresizingMaskIntoConstraints = false
+    panGesture = UIPanGestureRecognizer(target: self, action: #selector(panCard))
+    cardView.addGestureRecognizer(panGesture)
     NSLayoutConstraint.activate([
       cardView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
       cardView.widthAnchor.constraint(equalTo: cardView.heightAnchor, multiplier: 0.66),
       cardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
       view.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: 50),
     ])
+  }
+  
+  @objc func panCard(_ sender: UIPanGestureRecognizer) {
+    guard let card = sender.view else { return }
+    let point = sender.translation(in: view)
+    card.center = CGPoint(x: view.center.x + point.x,
+                          y: view.center.y + point.y)
   }
 }
 
