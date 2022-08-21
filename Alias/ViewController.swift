eaf80f4e9b4ed6ab2cefe80126ecc010faa5ddff
Vladimir <motiw@icloud.com>
2022-08-21 07:45:40 +0600

    Добавил картинку пальца вверх

--- Alias/ViewController.swift
+++ Alias/ViewController.swift
@@ -10,18 +10,31 @@ import SwiftUI
 class ViewController: UIViewController {
   let cardView = UIView()
   var panGesture = UIPanGestureRecognizer()
+  let thumbImageView: UIImageView = {
+    $0.translatesAutoresizingMaskIntoConstraints = false
+    $0.contentMode = .scaleAspectFit
+    return $0
+  }(UIImageView(image: UIImage(systemName: "hand.thumbsup.fill")?.withTintColor(.green, renderingMode: .alwaysOriginal)))
   override func viewDidLoad() {
     super.viewDidLoad()
     view.addSubview(cardView)
+    cardView.addSubview(thumbImageView)
+
     cardView.backgroundColor = .systemBlue
     cardView.translatesAutoresizingMaskIntoConstraints = false
     panGesture = UIPanGestureRecognizer(target: self, action: #selector(panCard))
     cardView.addGestureRecognizer(panGesture)
+    
     NSLayoutConstraint.activate([
       cardView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
       cardView.widthAnchor.constraint(equalTo: cardView.heightAnchor, multiplier: 0.66),
       cardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
       view.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: 50),
+      
+      thumbImageView.centerYAnchor.constraint(equalTo: cardView.centerYAnchor),
+      thumbImageView.centerXAnchor.constraint(equalTo: cardView.centerXAnchor),
+      thumbImageView.widthAnchor.constraint(equalTo: cardView.widthAnchor, multiplier: 0.5),
+      thumbImageView.heightAnchor.constraint(equalTo: thumbImageView.widthAnchor)
     ])
   }
   
